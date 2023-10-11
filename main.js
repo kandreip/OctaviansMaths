72bc0e036e8b35d7b8d8c63d65afdf00ffb4b741
Andrei Cozma <andreicozma89@yahoo.com>
2023-10-11 15:49:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -133,8 +133,9 @@ function generateOperations(max, min, sign) {
     let rightAnswer = document.createElement('div');
     rightAnswer.className = 'px-4 py-2 rounded-md text-white right-answer hidden';
     rightAnswer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="w-6 h-6">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
   </svg>
+  
   `;
     listItem.appendChild(rightAnswer);
 
